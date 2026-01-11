b434d743b0d2406b2bc6faf2709b8198600a0ebb
sondip dey <adrtadsx@gmail.com>
2026-01-12 04:43:16 +0600

    modified:   Greedy/Active_Section_02.java

--- Greedy/Active_Section_02.java
+++ Greedy/Active_Section_02.java
@@ -2,7 +2,7 @@ package Greedy;
 
 import java.util.*;
 
-public class Classroom{
+public class Active_Section_02{
     public static void main(String args[]){
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[] = {2, 4, 6, 7, 9, 9};
