4707ba481316e09429b69c6dd1770d70dd427c17
sondip dey <adrtadsx@gmail.com>
2026-01-14 01:37:03 +0600

    new file:   Greedy/Minimum_sum.java

---
+++ Greedy/Minimum_sum.java
@@ -0,0 +1,22 @@
+package Greedy;
+import java.util.*;
+
+
+class Minimum_sum {
+    public static void main(String args[]){
+        int A[] = {4, 1, 8, 7};
+        int B[] = {2, 3, 6, 5};
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+        
+        for(int i=0; i<A. length; i++){
+            minDiff += Math.abs(A[i]-B[i]);
+        }
+
+        System.out.println("Min absolute diff of pairs = " + minDiff);
+    }
+    
+}
